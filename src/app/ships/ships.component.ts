69702988d200c08a8bc6cead4399a522cc593c53
Pablo Pollono <pablo@vreasy.com>
2020-10-06 14:22:43 +0200

    Add Ships Page Component

---
+++ src/app/ships/ships.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ships',
+  templateUrl: './ships.component.html',
+  styleUrls: ['./ships.component.scss']
+})
+export class ShipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
