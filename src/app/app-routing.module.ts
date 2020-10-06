0fcb780e948322b351af469c01fb3215a360e0b4
Pablo Pollono <pablo@vreasy.com>
2020-10-06 14:30:18 +0200

    Add Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { ShipsComponent } from './ships/ships.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuardService } from './auth-guard.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'ships',
+    pathMatch: 'full',
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'ships',
+    component: ShipsComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
