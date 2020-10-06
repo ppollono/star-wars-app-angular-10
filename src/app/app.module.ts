0af95761c6b4efa6b280d7a37a4b0553e3389bdd
Pablo Pollono <pablo@vreasy.com>
2020-10-06 14:30:59 +0200

    Add Components to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ImgFallbackModule } from 'ngx-img-fallback';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ShipsComponent } from './ships/ships.component';
+import { ShipsListComponent } from './ships-list/ships-list.component';
+import { ShipComponent } from './ship/ship.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ShipsComponent,
+    ShipsListComponent,
+    ShipComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ImgFallbackModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
